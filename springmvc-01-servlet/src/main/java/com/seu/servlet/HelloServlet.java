6ca11fb91ee4a16b8f9c61170856064a4d4a189e
chenhuan <fengchenhuan@meituan.com>
2021-09-16 19:42:00 +0800

    fix:rebse git rebase --edit-todo
    本文来源：码农网
    本文链接：https://www.codercto.com/a/45325.html

--- springmvc-01-servlet/src/main/java/com/seu/servlet/HelloServlet.java
+++ springmvc-01-servlet/src/main/java/com/seu/servlet/HelloServlet.java
@@ -5,7 +5,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-//commit 1
+//commit 5
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
